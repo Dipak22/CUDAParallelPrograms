04a1aa24b6473c919364c32eb30371b635a4ec7f
Dipak Das <dipak.221190@gmail.com>
2025-03-02 20:15:34 +0530

    mat mul

---
+++ MatMul_Rowise.cu
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+#define WIDTH 4
+
+__global__ void matmul(int *A, int *B,int *C){
+    int row = blockIdx.x*blockDim.x + threadIdx.x;
+    if(row<WIDTH){
+        
+        for(int i=0;i<WIDTH;i++){
+            int result =0;
+            for(int j=0;j<WIDTH;j++){
+                result +=A[row*WIDTH +j] * B[j*WIDTH + i];
+            }
+            C[row*WIDTH + i] = result;
+        }
+    }
+}
+int main(){
+    int *A, *B, *C;
+    int memsize = WIDTH * WIDTH * sizeof(int);
+    C = (int*)malloc(memsize);
+    A = (int*)malloc(memsize);
+    B = (int*)malloc(memsize);
+    for(int i=0;i<WIDTH;i++){
+        for (int j = 0;j<WIDTH;j++){
+            A[i*WIDTH +j] = rand()%5;
+            B[i*WIDTH + j] = rand()%5;
+        }
+    }
+
+    int *A_d, *B_d, *C_d;
+    cudaMalloc((void**)&A_d, memsize);
+    cudaMalloc((void**)&B_d, memsize);
+    cudaMalloc((void**)&C_d, memsize);
+
+    cudaMemcpy(A_d, A, memsize, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B, memsize, cudaMemcpyHostToDevice);
+
+    matmul<<<ceil(WIDTH/16.0),16>>>(A_d,B_d,C_d);
+
+    cudaMemcpy(C,C_d,memsize, cudaMemcpyDeviceToHost);
+    cout<<"A"<<endl;
+    for(int i=0;i<WIDTH;i++){
+        for(int j=0;j<WIDTH;j++)
+            cout<<A[i*WIDTH +j]<<"  ";
+        cout<<"\n";
+    }
+    cout<<"B"<<endl;
+    for(int i=0;i<WIDTH;i++){
+        for(int j=0;j<WIDTH;j++)
+            cout<<B[i*WIDTH +j]<<"  ";
+        cout<<"\n";
+    }
+    cout<<"C"<<endl;
+    for(int i=0;i<WIDTH;i++){
+        for(int j=0;j<WIDTH;j++)
+            cout<<C[i*WIDTH +j]<<"  ";
+        cout<<"\n";
+    }
+
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);
+
+    free(A);
+    free(B);
+    free(C);
+
+    return 0;
+}
